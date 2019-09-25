41730881b7d9a074b6c40fb6e3ae6ef0d1e8ac9d
David <gubdev@protonmail.com>
2019-09-25 15:10:05 -0500

    Clue input + User Error
    added code for the user to input clues  and accounted for a few potential user errors

--- src/main/AnotherRunner.java
+++ src/main/AnotherRunner.java
@@ -16,6 +16,7 @@ public class AnotherRunner {
         //      User enters Name; it is kept within a string variable categoryName
         //      System asks user to enter each item within the category
         //      List of Nodes created based on user inputs
+        // PLEASE NOTE: I have not yet accounted for user error.(i.e. System: How many categories?| User: W| System: ERROR)
 
         Scanner scan = new Scanner(System.in);
         String numOfCat;
@@ -23,6 +24,9 @@ public class AnotherRunner {
         String catOrItemNum;
         String catName;
         String itemName;
+        String name1, name2, userInput;
+        Node a,b;
+        Boolean relationship, correctInput;
         int num1;
         int num2;
 
@@ -30,6 +34,14 @@ public class AnotherRunner {
 
         System.out.println("Hello! Enter the number of categories.");
         numOfCat = scan.next();
+
+        if(!isNumber(numOfCat)){ // Used to check is the user has input a number or not.
+            do{
+                System.out.println("I'm sorry, but that doesn't seem to be a number. \n" +
+                        "Please enter the NUMBER of categories.");
+                numOfCat = scan.next();
+            }while(!isNumber(numOfCat));
+        }
 
         for(int i = 0; i < Integer.parseInt(numOfCat); i++){
             //Sorry about this guys. Perfectionism tends to get the better of me.
@@ -52,6 +64,14 @@ public class AnotherRunner {
 
             System.out.println("Great! Now enter the number of items within this category.");
             numOfItems = scan.next();
+
+            if(!isNumber(numOfItems)){ // Used to check is the user has input a number or not.
+                do{
+                    System.out.println("I'm sorry, but that doesn't seem to be a number. \n" +
+                            "Please enter the NUMBER of items.");
+                    numOfItems = scan.next();
+                }while(!isNumber(numOfItems));
+            }
 
             for(int j = 0; j < Integer.parseInt(numOfItems); j++) {
                 if (j == 0) {
@@ -80,10 +100,77 @@ public class AnotherRunner {
         WeightedGraph wg = new WeightedGraph(list);
         wg.printGraph();
 
-        //Now that's done, we need to find a way to input the clues and have them transfer to the weights of the graph.
+        // Now that's done, we need to find a way to input the clues and have them transfer to the weights of the graph.
+        // Option 1:
+        //          User inputs the name of one item, then the name of another item and sets the connection to true of false
+        // Option 2:
+        //          User can input all three at once separated by commas
+
+        // Below is implementation of option 1
+        // Still need to implement a loop so that this keeps going until all clues are input or all connections are made.
 
         System.out.println("Great! Now that is done, time for us to input the clues you have!");
-        System.out.println("");
+        System.out.print("We will have you input these by first typing the name of one item, then the other, and " +
+                "finally, setting their relationship to either true or false.\n");
 
+        //______This is where the main "Clue Input" loop will begin and take place.
+        // Not writing now so that program can run.
+        // Will update with updated code from WeightedGraph so that it loops until all connections are made.
+        System.out.print("Please input the name of the first item in the relationship.");
+        name1 = scan.next();
+        System.out.print("Please input the name of the second item in the relationship.");
+        name2 = scan.next();
+        System.out.println("Are "+ name1 +" and " +  name2 + " related? (Please input \"yes\", \"y\", \"true\", or \"t\" for " +
+                "true. Please input \"no\", \"n\", \"false\", or \"f\" for false.)");
+        do {
+            userInput = scan.next();
+            if (userInput.equalsIgnoreCase("n") || userInput.equalsIgnoreCase("no")
+                    || userInput.equalsIgnoreCase("false") || userInput.equalsIgnoreCase("f")) {
+                relationship = false;
+                correctInput = true;
+            } else if (userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes")
+                    || userInput.equalsIgnoreCase("true") || userInput.equalsIgnoreCase("t")) {
+                relationship = true;
+                correctInput = true;
+            } else {
+                correctInput = false;
+                System.out.println("Sorry, " + userInput +" is not a valid input.");
+                System.out.println("Are "+ name1 +" and " +  name2 + " related? (Please input \"yes\", \"y\", \"true\", or \"t\" for " +
+                        "true. Please input \"no\", \"n\", \"false\", or \"f\" for false.)");
+            }
+        }while(!correctInput);
+        // Now that I have those inputs, we need to find the two nodes alluded to be the user.
+
+        // Possibility for below code to fail if the item name user input was misspelled or purposefully incorrect,
+        // therefore not found within the array list.
+        for(int k = 0; k < arrayListOfItems.size(); k++){
+            if(arrayListOfItems.get(k).getName() == name1 ){
+                a = arrayListOfItems.get(k);
+            }else if(arrayListOfItems.get(k).getName() == name2 ){
+                b = arrayListOfItems.get(k);
+            }
+        }// this should select the correct Nodes
+
+        // Time to take those two nodes and set their relationship.x
+        // This is using the code Matt is working on in the matt2 branch.
+        // Commented out for now until we merge all branches to master
+
+        // wg.addConnection(a, b, relationship, true);
+        System.out.println("Connection has been made. Please view the changes below");
+        wg.printGraph();
+        System.out.println("Time to add the next connection.");
+        //______End of Loop
+
+        //after this, we can create a nice print out of all of the connections to end the program.
+
+    }
+
+    public static boolean isNumber(String strNum) {
+        try {
+            int d = Integer.parseInt(strNum);
+        } catch (NumberFormatException | NullPointerException nfe) {
+            return false;
+        }
+        return true;
     }
 }
